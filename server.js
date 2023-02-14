ff2d987ceaf9405697fefef8669c6d636d5e5ff4
Harshavardhan Bammidi <73329321+harshavb08@users.noreply.github.com>
2023-02-14 21:02:18 +0530

    resume update

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 5010;
+const port = process.env.PORT || 3000;
 
-app.get("/", function(req, res){
-	res.send("I'm Harsha");
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html');
 });
 
-app.listen(port, function(){
-	console.log("Server running on http://localhost:"+port);
-	console.log(`Server running on http://localhost:${port}`);
+app.listen(port, function() {
+    console.log('Server started on port ' + port);
 });
