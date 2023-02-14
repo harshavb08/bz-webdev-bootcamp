939fd11345d7e25b32cad566b66c225efc3da246
Harshavardhan Bammidi <73329321+harshavb08@users.noreply.github.com>
2023-02-14 21:52:33 +0530

    resume link update

--- server.js
+++ server.js
@@ -2,8 +2,11 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-
 app.get('/', function(req, res) {
+    res.send("I'm Harsha");
+});
+
+app.get('/resume', function(req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
