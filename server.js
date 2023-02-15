2883865a2a8c4fe4c9eec25431d9ce240792d102
harshavb08 <harshavb08@gmail.com>
2023-02-15 20:50:31 +0530

    index page and card page updated

--- server.js
+++ server.js
@@ -2,12 +2,18 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', function(req, res) {
-    res.send("I'm Harsha");
+    res.sendFile(__dirname + '/index.html');
 });
 
 app.get('/resume', function(req, res) {
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/resume.html');
+});
+
+app.get('/card', function(req, res) {
+    res.sendFile(__dirname + '/card.html');
 });
 
 app.listen(port, function() {
