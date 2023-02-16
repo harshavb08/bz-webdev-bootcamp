bb0d33f9cb03fa6304bb2fced1a5acc2f8b822ca
harshavb08 <harshavb08@gmail.com>
2023-02-16 19:55:36 +0530

    crud operations updated

--- backend/lib/userLib.js
+++ backend/lib/userLib.js
@@ -1,14 +1,8 @@
 const userModel = require("../models/userModel");
 
-module.exports.getAllUsers = async function(callback) {
-    try {
-        var users = await userModel.find({});
-        callback(null, users);
-    } catch (err) {
-        callback(err, null);
-    }
-}
+// CREATE
 
+// creates first user
 module.exports.createFirstUser = async function(callback) {
     try {
         var user = {
@@ -18,6 +12,67 @@ module.exports.createFirstUser = async function(callback) {
         var newUser = new userModel(user);
         var result = await newUser.save();
         callback(null, result);
+    } catch (err) {
+        callback(err, null);
+    }
+}
+
+// create a user
+module.exports.createUser = async function(user, callback) {
+    try {
+        var newUser = new userModel(user);
+        var result = await newUser.save();
+        callback(null, result);
+    } catch (err) {
+        callback(err, null);
+    }
+}
+
+// READ
+
+// get all users
+module.exports.getAllUsers = async function(callback) {
+    try {
+        var users = await userModel.find({});
+        callback(null, users);
+    } catch (err) {
+        callback(err, null);
+    }
+}
+
+// get user by userName
+module.exports.getUserByUserName = async function(userName, callback) {
+    try {
+        var query = { userName: userName };
+        var user = await userModel.find(query);
+        callback(null, user);
+    } catch (err) {
+        callback(err, null);
+    }
+}
+
+// UPDATE
+
+// update values by userName
+module.exports.updateUserByUserName = async function(userName, newValues, callback) {
+    try {
+        var query = { userName: userName };
+        var updatedUser = await userModel.findOneAndUpdate(query, newValues, { new: true });
+        callback(null, updatedUser);
+    } catch (err) {
+        callback(err, null);
+    }
+}
+
+// DELETE   
+
+// delete user by userName
+
+module.exports.deleteUserByUserName = async function(userName, callback) {
+    try {
+        var query = { userName: userName };
+        var deletedUser = await userModel.findOneAndDelete(query);
+        callback(null, deletedUser);
     } catch (err) {
         callback(err, null);
     }
--- server.js
+++ server.js
@@ -25,7 +25,8 @@ mongoose.connect(process.env.MONGO_CONNECTION_STRING, {}, function(err) {
         console.error(err);
     } else {
         console.log('Connected to database');
-        // TODO : donot create a user if atleast 1 user exist in the table
+        // TODO : donot create a user if atleast 1 user exist in the table - Done 
+        // Get all users
         userLib.getAllUsers(function(err, usersList) {
             if (err) {
                 console.error(err);
@@ -39,8 +40,80 @@ mongoose.connect(process.env.MONGO_CONNECTION_STRING, {}, function(err) {
                         }
                     });
                 }
+                console.log("Users found:");
+                console.log(usersList);
             }
         });
+
+        // Create a user
+        var user1 = {
+            userName: "kiran99",
+            yearOfGraduation: 2025,
+        };
+
+        var user2 = {
+            userName: "testUser",
+            yearOfGraduation: 2024,
+        };
+
+        // userLib.createUser(user1, function(err, res) {
+        //     if (err) {
+        //         console.error(err);
+        //     } else {
+        //         console.log("User created:");
+        //         console.log(res);
+        //     }
+        // });
+
+        // userLib.createUser(user2, function(err, res) {
+        //     if (err) {
+        //         console.error(err);
+        //     } else {
+        //         console.log("User created:");
+        //         console.log(res);
+        //     }
+        // });
+
+        //  Get user by userName
+
+        // userLib.getUserByUserName("harshavb08", function(err, res) {
+        //     if (err) {
+        //         console.error(err);
+        //     } else {
+        //         console.log("User found by userName:");
+        //         console.log(res);
+        //     }
+        // });
+
+        // Update user by userName
+
+        var newValues = {
+            $set: {
+                yearOfGraduation: 2026,
+            }
+        };
+
+        // userLib.updateUserByUserName("harshavb08", newValues, function(err, res) {
+        //     if (err) {
+        //         console.error(err);
+        //     } else {
+        //         console.log("User updated by userName:");
+        //         console.log(res);
+        //     }
+        // });
+
+        //delete user by userName
+
+        // userLib.deleteUserByUserName("testUser", function(err, res) {
+        //     if (err) {
+        //         console.error(err);
+        //     } else {
+        //         console.log("User deleted by userName:");
+        //         console.log(res);
+        //     }
+        // });
+
+
         app.listen(port, function() {
             console.log('Server started on port ' + port);
         });
